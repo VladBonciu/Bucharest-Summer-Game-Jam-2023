72266983d32a287b4e189f8c1c3c7648402effe1
rudeDoodle <110019148+rudeDoodle@users.noreply.github.com>
2023-07-29 14:50:01 +0300

    e

---
+++ Assets/CameraControl.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+public class CameraControl : MonoBehaviour
+{
+    public CinemachineVirtualCamera vcam1; //BombCamera 
+    public CinemachineVirtualCamera vcam2; //PillCamera
+    public CinemachineVirtualCamera vcam3; //InstructionCamera
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) 
+        {
+            vcam1.Priority = 1;
+            vcam2.Priority = 0;
+            vcam3.Priority = 0;
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            vcam1.Priority = 0;
+            vcam2.Priority = 1;
+            vcam3.Priority = 0;
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            vcam1.Priority = 0;
+            vcam2.Priority = 0;
+            vcam3.Priority = 1;
+
+        }
+    }
+}
